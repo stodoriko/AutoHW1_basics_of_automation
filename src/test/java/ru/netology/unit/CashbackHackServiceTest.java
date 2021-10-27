b3fd7364a118b3e23787e7fc973fb3addf1cd078
stodoriko <s.todorikos@gmail.com>
2021-10-27 19:11:45 +0300

    rename test class into junit4-platform branch

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-public class BonusServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
     public void shouldShowHowMuchToBuy() {
